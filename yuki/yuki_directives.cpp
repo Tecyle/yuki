853055be6a83bc443b0c9c1725cbe17a2cfa05ef
Tecyle <tecyle@163.com>
2017-09-14 20:20:36 +0800

    WIP.

--- yuki/yuki_directives.cpp
+++ yuki/yuki_directives.cpp
@@ -4,3 +4,61 @@
 #include "yuki_file_reader.h"
 #include "yuki_line_string.h"
 #include "yuki_directives.h"
+#include "yuki_simple_reference_name.h"
+
+bool YukiDirectives::parse(YukiNode* parentNode, const YukiRegion* region)
+{
+	YukiFileReader* reader = getFileReader();
+	yuki_cursor oldCursor = reader->getCursor();
+	const YukiRegion* oldRegion = reader->selectRegion(region);
+	bool succ = false;
+
+	if (!matchNoBackward())
+		goto match_finished;
+
+	succ = true;
+	wstring directive = m_directiveName + +L"-directive";
+	YukiStruct* parser = getParser(directive.c_str());
+	if (parser != nullptr)
+	{
+		parser->parse(parentNode, reader->cutRegionFromCursorToEnd());
+	}
+	else
+	{
+		getParser(L"reserved_text")->parse(parentNode, reader->cutRegionFromCursorToEnd());
+	}
+
+match_finished:
+	if (!succ)
+		reader->setCursor(oldCursor);
+	reader->selectRegion(oldRegion);
+	return succ;
+}
+
+bool YukiDirectives::match()
+{
+	YukiFileReader* reader = getFileReader();
+	yuki_cursor oldCursor = reader->getCursor();
+
+	bool succ = matchNoBackward();
+
+	reader->setCursor(oldCursor);
+	return succ;
+}
+
+bool YukiDirectives::matchNoBackward()
+{
+	YukiFileReader* reader = getFileReader();
+	YukiSimpleReferenceName* simpleRefName = dynamic_cast<YukiSimpleReferenceName*>(getParser(L"simple_reference_name"));
+
+	if (!simpleRefName->parseReferenceName(&m_directiveName))
+		return false;
+
+	if (!reader->matchStr(L"::"))
+		return false;
+
+	if (reader->skipSpaces() < 1)
+		return false;
+
+	return true;
+}
--- yuki/yuki_directives.h
+++ yuki/yuki_directives.h
@@ -7,4 +7,52 @@
 		<option list>
 
 		<body>
+
+	识别要点：
+	1. 识别到 simple-reference-name 后面紧跟 ``::`` 以及空格就算识别成功
+	2. 第一行如果有内容，则识别为 <arguments>，如果设定不接受 <arguments>，
+	   则直接识别为 body
+	3. 开始识别 <arguments> 之后，没有遇到空行的前提下，第二行的内容尝试识
+	   别为 <option list>，如果失败则继续识别为 <arguments>，并且第二行的
+	   缩进开始计算公共缩进
+	4. 继续步骤 3，没有遇到空行的前提下，如果遇到的行小于等于 <arguments>
+	   的公共缩进，则尝试识别为 <option list>，否则继续识别为 <arguments>
+	5. 识别为 <option list> 之后，后续不属于 <option list> 部分的内容全都
+	   属于 <body> 内容
+	6. 如果设定不接受 <option list>，则空行之前的部分全都作为 <arguments>
+	   识别
+	7. 如果第一行没有内容，则第二行开始，尝试识别 <option list>，如果失败，
+	   并且接受 <arguments>，则识别为 <arguments>，否则识别为 <body>；如果
+	   成功，则识别为 <option list>，并认为 <arguments> 没有给出。
+
+	如果遇到了未定义的指令，则整体内容均作为预排文本块输出。
 */
+
+// 指令分配器
+class YukiDirectives : public YukiStruct
+{
+public:
+	virtual bool parse(YukiNode* parentNode, const YukiRegion* region) override;
+	virtual bool match() override;
+
+protected:
+	virtual bool matchNoBackward();
+
+protected:
+	wstring m_directiveName;
+};
+
+// 指令基类，所有指令必须命名为 指令名称 + ``-directive``，比如 ``image-directive``
+class YukiDirective : public YukiStruct
+{
+public:
+	virtual bool parse(YukiNode* parentNode, const YukiRegion* region) override;
+	virtual bool match() override;
+
+	virtual bool matchNoBackward();
+
+protected:
+	bool m_acceptArguments;
+	bool m_acceptOptionLists;
+	bool m_acceptBody;
+};
