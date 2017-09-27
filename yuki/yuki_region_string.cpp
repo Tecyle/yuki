33378fb853d71721e887964040ae218d811bc992
Tecyle <tecyle@163.com>
2017-09-27 17:15:27 +0800

    WIP.

--- yuki/yuki_region_string.cpp
+++ yuki/yuki_region_string.cpp
@@ -7,10 +7,29 @@
 #include "yuki_file_string.h"
 #include "yuki_line_string_manager.h"
 
+static int _countRegionLines(yuki_file_string* fileString, const yuki_region* region)
+{
+	if (region->end().isValid())
+		return region->end().ln - region->begin().ln;
+	return fileString->getLineCount() - region->begin().ln;
+}
+
 yuki_region_string::yuki_region_string(yuki_file_string* parent, const yuki_region* region)
 	: m_parent(parent)
 	, m_region(region)
 	, m_lineCount(0)
 	, m_lines(nullptr)
 {
+}
+
+void yuki_region_string::buildLineStringArray()
+{
+	auto mgr = yukiLineStringManager();
+	m_lineCount = _countRegionLines(m_parent, m_region);
+	m_lines = static_cast<yuki_line_string**>(malloc(sizeof(yuki_line_string*) * m_lineCount));
+
+	for (int i = 0; i < m_lineCount; ++i)
+	{
+		m_lines[i] = mgr->allocLineStringForRegionString(this, i);
+	}
 }
